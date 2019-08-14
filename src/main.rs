2124213c63c9ab56118f3d4a533557b12720573a
Simon Martin <smartin@scottlogic.com>
2019-08-14 07:18:56 +0000

    Convert methods to just return vector of groups

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ use std::io::SeekFrom;
 
 const BLOCK_SIZE: usize = 1024;
 
-fn find_files(sourceroot: std::ffi::OsString, recursive: bool) -> BTreeMap<u64, Vec<String>> {
+fn find_files(sourceroot: std::ffi::OsString, recursive: bool) -> Vec<Vec<String>> {
+    info!("find all files in {:?} (recursive: {})", sourceroot, recursive);
+
     let walk = WalkDir::new(sourceroot);
     let walk = match recursive {
         true => walk.into_iter(),
@@ -28,11 +30,11 @@ fn find_files(sourceroot: std::ffi::OsString, recursive: bool) -> BTreeMap<u64, 
         .fold(BTreeMap::new(), |mut acc, entry| {
             acc.entry(entry.0).or_insert(Vec::new()).push(entry.1);
             acc
-        })
+        }).values().cloned().collect()
 }
 
-fn remove_uniq<K: std::cmp::Ord>(groups: BTreeMap<K, Vec<String>>) -> BTreeMap<K, Vec<String>> {
-    groups.into_iter().filter(|(_, value)| value.len() > 1).collect()
+fn remove_uniq(groups: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    groups.into_iter().filter(|value| value.len() > 1).collect()
 }
 
 fn gen_partial_crc(filename:&str) -> io::Result<(String, u16)> {
@@ -43,16 +45,16 @@ fn gen_partial_crc(filename:&str) -> io::Result<(String, u16)> {
     Ok((filename.to_string(), crc16::checksum_usb(&buffer)))
 }
 
-fn gen_partial_crcs(groups: BTreeMap<u64, Vec<String>>) -> BTreeMap<(u64, u64), Vec<String>> {
-    groups.into_iter().flat_map(|(size, group)| {
+fn gen_partial_crcs(groups: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    groups.into_iter().flat_map(|group| {
         group.into_iter()
         .map(|filename| gen_partial_crc(&filename))
         .filter_map(Result::ok)
         .fold(BTreeMap::new(), |mut acc, (filename, crc)| {
-            acc.entry((size, crc as u64)).or_insert(Vec::new()).push(filename);
+            acc.entry(crc as u64).or_insert(Vec::new()).push(filename);
             acc
         })
-    }).collect()
+    }).map(|(_, group)| group).collect()
 }
 
 fn gen_full_crc(filename:&str) -> io::Result<(String, u16)> {
@@ -75,16 +77,16 @@ fn gen_full_crc(filename:&str) -> io::Result<(String, u16)> {
     Ok((filename.to_string(), digest.sum16()))
 }
 
-fn gen_full_crcs(groups: BTreeMap<(u64, u64), Vec<String>>) -> BTreeMap<(u64, u64), Vec<String>> {
-    groups.into_iter().flat_map(|(key, group)| {
+fn gen_full_crcs(groups: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    groups.into_iter().flat_map(|group| {
         group.into_iter()
         .map(|filename| gen_full_crc(&filename))
         .filter_map(Result::ok)
         .fold(BTreeMap::new(), |mut acc, (filename, crc)| {
-            acc.entry((key.0, crc as u64)).or_insert(Vec::new()).push(filename);
+            acc.entry(crc as u64).or_insert(Vec::new()).push(filename);
             acc
         })
-    }).collect()
+    }).map(|(_, group)| group).collect()
 }
 
 fn open_files(size: u64, group: Vec<String>) -> BTreeMap<u64, Vec<(String, std::fs::File)>> {
@@ -117,18 +119,18 @@ fn read_block(size: u64, group: Vec<(String, std::fs::File)>, block_start: usize
     })
 }
 
-fn partition_group_by_bytes(size: u64, group: Vec<String>) -> BTreeMap<(u64, u64), Vec<String>> {
+fn partition_group_by_bytes(group: Vec<String>) -> Vec<Vec<String>> {
     group.into_iter()
     .fold(BTreeMap::new(), |mut acc, filename| {
         let group: u64 = 0;
-        acc.entry((size, group)).or_insert(Vec::new()).push(filename);
+        acc.entry(group).or_insert(Vec::new()).push(filename);
         acc
-    })
+    }).into_iter().map(|(_, group)| group).collect()
 }
 
-fn byte_match(groups: BTreeMap<(u64, u64), Vec<String>>) -> BTreeMap<(u64, u64), Vec<String>> {
-    groups.into_iter().flat_map(|((size, _), group)| {
-        partition_group_by_bytes(size, group)
+fn byte_match(groups: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    groups.into_iter().flat_map(|group| {
+        partition_group_by_bytes(group)
     }).collect()
 }
 
@@ -142,18 +144,22 @@ fn main() {
     let groups = find_files(sourceroot, recursive);
     // Remove files with unique size
     let groups = remove_uniq(groups);
+    debug!("{} non-unique groups (by size)", groups.len());
 
     // Remove files with unique partial crc
     let groups = gen_partial_crcs(groups);
     let groups = remove_uniq(groups);
+    debug!("{} non-unique groups (by partial crc)", groups.len());
 
     // Remove files with unique full crc
     let groups = gen_full_crcs(groups);
     let groups = remove_uniq(groups);
+    debug!("{} non-unique groups (by full crc)", groups.len());
 
     // Remove files with unique bytes
     let groups = byte_match(groups);
     let groups = remove_uniq(groups);
+    debug!("{} non-unique groups (by exact content)", groups.len());
 
     for bucket in groups.iter().rev() {
       debug!("{:#?}", bucket);
