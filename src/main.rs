daf6992606eeac968172294b2b0c67313098511e
Simon Martin <smartin@scottlogic.com>
2020-02-10 13:52:25 +0000

    clippy lint cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::io::SeekFrom;
 use env_logger::{Builder,Env};
-use log::LevelFilter;
 use chrono::Local;
 
 const BLOCK_SIZE: usize = 1024;
@@ -28,9 +27,10 @@ fn find_files(sourceroot: std::ffi::OsString, recursive: bool, skip_empty: bool)
     info!("find all files in {:?} (recursive: {}, skip_empty: {})", sourceroot, recursive, skip_empty);
 
     let walk = WalkDir::new(sourceroot);
-    let walk = match recursive {
-        true => walk.into_iter(),
-        false => walk.max_depth(1).into_iter()
+    let walk = if recursive {
+        walk.into_iter()
+    } else {
+        walk.max_depth(1).into_iter()
     };
 
     walk
@@ -40,7 +40,7 @@ fn find_files(sourceroot: std::ffi::OsString, recursive: bool, skip_empty: bool)
         .fold(BTreeMap::new(), |mut acc, entry| {
             let size = entry.0;
             if size > 0 || !skip_empty {
-                acc.entry(size).or_insert(Vec::new()).push(FdupesFile {filename: entry.1, size});
+                acc.entry(size).or_insert_with(Vec::new).push(FdupesFile {filename: entry.1, size});
             }
             acc
         }).values().cloned().collect()
@@ -61,10 +61,10 @@ fn gen_partial_crc(file:FdupesFile) -> io::Result<(FdupesFile, u16)> {
 fn gen_partial_crcs(groups: Vec<Vec<FdupesFile>>) -> Vec<Vec<FdupesFile>> {
     groups.into_iter().flat_map(|group| {
         group.into_iter()
-        .map(|file| gen_partial_crc(file))
+        .map(gen_partial_crc)
         .filter_map(Result::ok)
         .fold(BTreeMap::new(), |mut acc, (filename, crc)| {
-            acc.entry(crc as u64).or_insert(Vec::new()).push(filename);
+            acc.entry(crc as u64).or_insert_with(Vec::new).push(filename);
             acc
         })
     }).map(|(_, group)| group).collect()
@@ -93,10 +93,10 @@ fn gen_full_crc(file:FdupesFile) -> io::Result<(FdupesFile, u16)> {
 fn gen_full_crcs(groups: Vec<Vec<FdupesFile>>) -> Vec<Vec<FdupesFile>> {
     groups.into_iter().flat_map(|group| {
         group.into_iter()
-        .map(|file| gen_full_crc(file))
+        .map(gen_full_crc)
         .filter_map(Result::ok)
         .fold(BTreeMap::new(), |mut acc, (file, crc)| {
-            acc.entry(crc as u64).or_insert(Vec::new()).push(file);
+            acc.entry(crc as u64).or_insert_with(Vec::new).push(file);
             acc
         })
     }).map(|(_, group)| group).collect()
@@ -106,9 +106,8 @@ fn open_files(size: u64, group: Vec<String>) -> BTreeMap<u64, Vec<(String, std::
     group.into_iter()
     .fold(BTreeMap::new(), |mut acc, filename| {
         let nom = filename.clone();
-        match File::open(filename) {
-            Ok(file) => acc.entry(size).or_insert(Vec::new()).push((nom, file)),
-            Err(_) => {}
+        if let Ok(file) = File::open(filename) {
+            acc.entry(size).or_insert_with(Vec::new).push((nom, file));
         }
         acc
     })
@@ -124,10 +123,9 @@ fn read_file_block(file: &mut std::fs::File, block_start: usize) -> io::Result<(
 fn read_block(size: u64, group: Vec<(String, std::fs::File)>, block_start: usize) -> BTreeMap<u64, Vec<(String, std::fs::File, usize, [u8; BLOCK_SIZE])>> {
     group.into_iter()
     .fold(BTreeMap::new(), |mut acc, (filename, mut file)| {
-        match read_file_block(&mut file, block_start) {
-            Ok((read, buffer)) => acc.entry(size).or_insert(Vec::new()).push((filename, file, read, buffer)),
-            Err(_) => {}
-        };
+        if let Ok((read, buffer)) = read_file_block(&mut file, block_start) {
+            acc.entry(size).or_insert_with(Vec::new).push((filename, file, read, buffer));
+        }
         acc
     })
 }
@@ -136,7 +134,7 @@ fn partition_group_by_bytes(group: Vec<FdupesFile>) -> Vec<Vec<FdupesFile>> {
     group.into_iter()
     .fold(BTreeMap::new(), |mut acc, file| {
         let group: u64 = 0;
-        acc.entry(group).or_insert(Vec::new()).push(file);
+        acc.entry(group).or_insert_with(Vec::new).push(file);
         acc
     }).into_iter().map(|(_, group)| group).collect()
 }
@@ -186,11 +184,8 @@ fn main() {
     let groups = remove_uniq(groups);
     info!("{} non-unique groups (by exact content)", groups.len());
 
-    let groups = {
-        let mut ngroups = groups.clone();
-        ngroups.sort_unstable_by(|b, a| a.get(0).map(|f| f.size + 1).unwrap_or(0).cmp(&b.get(0).map(|f| f.size + 1).unwrap_or(0)));
-        ngroups
-    };
+    let mut groups = groups;
+    groups.sort_unstable_by(|b, a| a.get(0).map(|f| f.size + 1).unwrap_or(0).cmp(&b.get(0).map(|f| f.size + 1).unwrap_or(0)));
 
     for bucket in groups.iter() {
       debug!("{:#?}", bucket);
