990da485e6d1b20fc12f613089cee555f4e1f23b
Simon Martin <havvoric@gmail.com>
2022-02-01 19:57:42 +0000

    Migration into separated code files.

---
+++ src/dupemessage.rs
@@ -0,0 +1,12 @@
+use crate::fdupesgroup::FdupesGroup;
+
+pub enum DupeMessage {
+    End,
+    Group(u64, Vec<String>)
+}
+
+impl std::convert::From<FdupesGroup> for DupeMessage {
+    fn from(group: FdupesGroup) -> Self {
+        Self::Group(group.size, group.filenames)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate log;
 extern crate chrono;
 extern crate env_logger;
@@ -6,106 +5,35 @@ extern crate serde;
 
 use chrono::Local;
 use env_logger::{Builder, Env};
-use std::collections::BTreeMap;
 use std::env;
-use std::io;
 use std::io::prelude::*;
-use walkdir::WalkDir;
 
-use std::sync::atomic::Ordering;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 
-use fdupes::FdupesGroup;
+use num_format::{Locale, ToFormattedString};
 
-fn find_files(
-    sourceroot: Vec<std::ffi::OsString>,
-    recursive: bool,
-    skip_empty: bool,
-) -> BTreeMap<u64, Vec<String>> {
-    info!(
-        "find all files in {:?} (recursive: {}, skip_empty: {})",
-        sourceroot, recursive, skip_empty
-    );
-    let all_groups = sourceroot
-        .iter()
-        .flat_map(|root| {
-            info!("scanning {:?}...", root);
-            let walk = WalkDir::new(root);
-            if recursive {
-                walk.into_iter()
-            } else {
-                walk.max_depth(1).into_iter()
-            }
-        })
-        .map(std::result::Result::unwrap)
-        .filter(|entry| !entry.path().is_symlink())
-        .filter(|entry| entry.path().is_file())
-        .map(|entry| {
-            (
-                entry.metadata().unwrap().len(),
-                entry.path().to_str().unwrap_or("").to_string(),
-            )
-        })
-        .fold(BTreeMap::new(), |mut acc, entry| {
-            let size = entry.0;
-            if size > 0 || !skip_empty {
-                acc.entry(size).or_insert_with(Vec::new).push(entry.1);
-            }
-            acc
-        });
-    info!("{} non-unique groups (by size)", all_groups.len());
-    all_groups
-        .into_iter()
-        .filter(|(_size, files)| files.len() > 1)
-        .collect()
-}
+use fdupes::DupeMessage;
+use fdupes::DupeScanner;
 
-fn remove_uniq(groups: Vec<FdupesGroup>) -> Vec<FdupesGroup> {
-    groups.into_iter().filter(|value| value.len() > 1).collect()
-}
-
-fn matches(file: &mut FdupesGroup, group: &mut FdupesGroup) -> io::Result<bool> {
-    trace!("Compared {file:?} vs {group:?}");
-    let filecrc = file.partialcrc()?;
-    let groupcrc = group.partialcrc()?;
-    if filecrc != groupcrc {
-        return Ok(false);
-    }
-    let filecrc = file.fullcrc()?;
-    let groupcrc = group.fullcrc()?;
-    if filecrc != groupcrc {
-        return Ok(false);
-    }
-    Ok(file == group)
-}
-
-fn update_matches(filename: &str, size: u64, result: &mut Vec<FdupesGroup>) {
-    let mut file = FdupesGroup::new(filename, size);
-    for r in result.iter_mut() {
-        if let Ok(true) = matches(&mut file, r) {
-            r.add(filename);
-            return;
+fn handle_group(size: u64, filenames: Vec<String>) {
+    if filenames.len() > 1 {
+        if size == 1 {
+            println!("{} byte each:", size.to_formatted_string(&Locale::en_GB));
+        } else {
+            println!("{} bytes each:", size.to_formatted_string(&Locale::en_GB));
         }
-    }
-    result.push(file);
-}
-
-fn build_matches(groups: BTreeMap<u64, Vec<String>>) -> Vec<FdupesGroup> {
-    let mut matches = Vec::new();
-    for (size, filenames) in groups.iter().rev() {
-        debug!("build matches {}: {} files", size, filenames.len());
-        let mut result = Vec::new();
-        for filename in filenames {
-            update_matches(filename, *size, &mut result);
+        for (id, filename) in filenames.iter().enumerate() {
+            println!("[{}] {} (W)", id + 1, filename);
         }
-        let mut result = remove_uniq(result);
-        debug!(" => {:?}", result.iter().map(|r|r.filenames.len()).collect::<Vec<_>>());
-        matches.append(&mut result);
-        debug!("matches has {} groups", matches.len());
+        println!("Preserve files [1 - {}, all, none, quit]", filenames.len());
+
+        let id = 0;
+        if let Some(filename) = filenames.get(id) {
+        }
+        println!();
     }
-    trace!("{} partial crc calcs", fdupes::GLOBAL_PARTIAL_CRC_COUNT.load(Ordering::SeqCst));
-    trace!("{} full crc calcs", fdupes::GLOBAL_FULL_CRC_COUNT.load(Ordering::SeqCst));
-    trace!("{} full file reads", fdupes::GLOBAL_FULL_READ_COUNT.load(Ordering::SeqCst));
-    matches
 }
 
 fn main() {
@@ -127,22 +55,24 @@ fn main() {
     let recursive = true;
     let skip_empty = true;
 
-    let groups = find_files(sourceroot, recursive, skip_empty);
-    info!("{} total groups (by size)", groups.len());
-    let groups = build_matches(groups);
+    let (tx, rx): (Sender<DupeMessage>, Receiver<DupeMessage>) = mpsc::channel();
 
-    let groups = remove_uniq(groups);
+    let finder = thread::spawn(move || {
+        let scanner = DupeScanner::new(tx, sourceroot, recursive, skip_empty);
+        scanner.find_groups();
+    });
 
-    for bucket in &groups {
-        if bucket.size == 1 {
-            println!("{} byte each:", bucket.size);
-        } else {
-            println!("{} bytes each:", bucket.size);
+    loop {
+        let group = rx.recv();
+        match group {
+            Ok(DupeMessage::End) => break,
+            Ok(DupeMessage::Group(size, group)) => handle_group(size, group),
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
         }
-        for filename in &bucket.filenames {
-            println!("{} (W)", filename);
-        }
-        println!();
     }
-    info!("{} non-unique groups (by exact content)", groups.len());
+
+    finder.join();
 }
