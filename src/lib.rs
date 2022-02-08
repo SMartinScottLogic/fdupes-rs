b76ffb1a6aea78043097d2de2da84e3bce1a8861
Simon Martin <smartin@scottlogic.com>
2022-02-08 19:00:54 +0000

    Add tests for crc comparison (#3)
    * Add tests for crc comparison
    
    * Adjust command line params
    
    * Clippy cleanup
    
    Co-authored-by: Simon Martin <havvoric@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,23 @@ mod scanner;
 
 pub mod receiver;
 
-#[derive(Debug, Clone)]
-#[derive(Parser)]
+#[derive(Debug, Clone, Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
     /// Only find duplicates immediately within supplied directories.
     #[clap(short, long)]
     pub non_recursive: bool,
     /// Should empty files be considered duplicates.
-    #[clap(short='0', long)]
+    #[clap(short = '0', long)]
     pub include_empty: bool,
     /// Show sizes of files within duplicate groups.
-    #[clap(short='z', long)]
+    #[clap(short='S', long)]
     pub show_sizes: bool,
     /// Path(s) to search for files within.
     pub root: Vec<String>,
+    /// prompt user for files to preserve and delete all others.
+    #[clap(short='p', long)]
+    pub prompt: bool,
 }
 
 pub use crate::dupemessage::DupeMessage;
