d6b36afbdcd1bf6738f36b41486b67d195e8a881
Simon Martin <havvoric@gmail.com>
2022-02-08 18:54:43 +0000

    Adjust command line params

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,13 @@ pub struct Config {
     #[clap(short='0', long)]
     pub include_empty: bool,
     /// Show sizes of files within duplicate groups.
-    #[clap(short='z', long)]
+    #[clap(short='S', long)]
     pub show_sizes: bool,
     /// Path(s) to search for files within.
     pub root: Vec<String>,
+    /// prompt user for files to preserve and delete all others.
+    #[clap(short='p', long)]
+    pub prompt: bool,
 }
 
 pub use crate::dupemessage::DupeMessage;
