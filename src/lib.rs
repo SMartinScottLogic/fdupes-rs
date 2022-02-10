a2bb9edabf2158414b3f75337812dc75cb64c21a
Simon Martin <smartin@scottlogic.com>
2022-02-10 20:22:47 +0000

    Add option to purge to trash (#4)
    Co-authored-by: Simon Martin <havvoric@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub mod receiver;
 #[derive(Debug, Clone, Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
+    /// Path(s) to search for files within.
+    pub root: Vec<String>,
     /// Only find duplicates immediately within supplied directories.
     #[clap(short, long)]
     pub non_recursive: bool,
@@ -17,11 +19,12 @@ pub struct Config {
     /// Show sizes of files within duplicate groups.
     #[clap(short='S', long)]
     pub show_sizes: bool,
-    /// Path(s) to search for files within.
-    pub root: Vec<String>,
     /// prompt user for files to preserve and delete all others.
     #[clap(short='p', long)]
     pub prompt: bool,
+    /// purge files into trash, rather than permanently.
+    #[clap(short='t', long)]
+    pub trash: bool,
 }
 
 pub use crate::dupemessage::DupeMessage;
